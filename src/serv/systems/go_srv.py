2fec3c5f7048fa325549743399657f6ab1de8ffa
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-05-01 14:50:47 -0700

    Refactor File Structure

---
+++ src/serv/systems/go_srv.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Process
+from services import ServiceNames, ServicePorts
+from req_resp import GenericRequest
+from server import start_server
+from go import Go1
+
+def generic_callback(request: GenericRequest, go: Go1):
+    function_call = getattr(go, request.function)
+    return function_call(**request.args)
+
+def start_go_server(name, port):
+    go = Go1()
+    try:
+        start_server(name=name, port=port, callback=lambda request: generic_callback(request, go))
+    except Exception as e:
+        print(e)
+
+def start_go(service_name, service_port):
+    process = Process(target=start_go_server, args=(service_name, service_port))
+    process.start()
+    try:
+        process.join()
+    except KeyboardInterrupt:
+        ...
+
+if __name__ == "__main__":
+    start_go(ServiceNames.GO, ServicePorts[ServiceNames.GO])
