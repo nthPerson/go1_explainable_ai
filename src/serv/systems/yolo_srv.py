b8e76cb7b707cece49dc893c0fab2a48feb30322
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-05-08 13:20:00 -0700

    Refactor Services To Generic Service

--- src/serv/systems/yolo_srv.py
+++ src/serv/systems/yolo_srv.py
@@ -1,29 +1,8 @@
 #!/usr/bin/env python3
 
-from multiprocessing import Process
-from services import ServiceNames, ServicePorts
-from req_resp import GenericRequest
-from server import start_server
 from yolo import PeopleDetector
-
-def generic_callback(request: GenericRequest, detector: PeopleDetector):
-    function_call = getattr(detector, request.function)
-    return function_call(**request.args)
-
-def start_detector_server(name, port):
-    detector = PeopleDetector()
-    try:
-        start_server(name=name, port=port, callback=lambda request: generic_callback(request, detector))
-    except Exception as e:
-        print(e)
-
-def start_detector(service_name, service_port):
-    process = Process(target=start_detector_server, args=(service_name, service_port))
-    process.start()
-    try:
-        process.join()
-    except KeyboardInterrupt:
-        ...
+from services import ServiceNames, ServicePorts
+from gen_srv import start_generic_server
 
 if __name__ == "__main__":
-    start_detector(ServiceNames.YOLO, ServicePorts[ServiceNames.YOLO])
+    start_generic_server(ServiceNames.YOLO, ServicePorts[ServiceNames.YOLO], PeopleDetector)
