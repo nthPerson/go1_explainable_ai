97c10fe7c5c23eef38cc257bc8ea1c283d26c50a
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-05-08 13:21:33 -0700

    Add Logic To Speech Processor

--- src/go/speech/speech_processor.py
+++ src/go/speech/speech_processor.py
@@ -3,17 +3,38 @@
 import pyttsx3
 import subprocess
 
+CUSTOM_PHRASE = 0
+
 class SpeechProcessor():
     def __init__(self):
         self.engine = pyttsx3.init()
         self.engine.setProperty('rate',125)
 
-    def speak(self, string: str):
-        self.engine.say(string)
-        self.engine.save_to_file(string, '/home/dicelabs/dog_py/src/go/speech/temp.wav')
+    def speak(self, phrase: str, file: str='/home/dicelabs/dog_py/src/go/speech/temp.wav'):
+        self.engine.say(phrase)
+        self.engine.save_to_file(phrase, file)
         self.engine.runAndWait()
-        # self.send_to_dog()
-    def send_to_dog(self):
+        if CUSTOM_PHRASE:
+            self.send_file_to_dog()
+        else:
+            if phrase == "Going To Next Target":
+                self.send_signal_to_dog("target.wav")
+            elif phrase == "Entering Search State":
+                self.send_signal_to_dog("search.wav")
+            elif phrase == "Human Detected":
+                self.send_signal_to_dog("human.wav")
+            elif phrase == "Starting Activity Recognition":
+                self.send_signal_to_dog("activity.wav")
+
+    def send_signal_to_dog(filename):
+        bash_script_path = "/home/dicelabs/dog_py/src/go/speech/dog_say.sh"
+        try:
+            subprocess.run(['bash', bash_script_path], args=[filename], check=True)
+            print("Bash script executed successfully")
+        except subprocess.CalledProcessError as e:
+            print("Error running Bash script:", e)
+
+    def send_file_to_dog(self):
         bash_script_path = "/home/dicelabs/dog_py/src/go/speech/dog_speak.sh"
         try:
             subprocess.run(['bash', bash_script_path], check=True)
@@ -23,4 +44,4 @@ class SpeechProcessor():
 
 if __name__ == "__main__":
     processor = SpeechProcessor()
-    processor.speak("I'm a robot talking dog, what else do you want bitch")
+    processor.speak("Human Detected")
